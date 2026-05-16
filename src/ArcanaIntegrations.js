e0ee74c30cdbd3bbf3f6903506983511ea910edb
stephdrealss <94701111+stephdrealss@users.noreply.github.com>
2026-05-16 01:18:08 +0100

    Update ArcanaIntegrations.js

--- src/ArcanaIntegrations.js
+++ src/ArcanaIntegrations.js
@@ -323,7 +323,7 @@ export function BridgePanel({ t, account }) {
   const SUPPORTED_CHAINS = [
     {id:"Ethereum_Sepolia",label:"Ethereum",icon:<img src="https://assets.coingecko.com/coins/images/279/small/ethereum.png" width="20" height="20" style={{borderRadius:"50%"}}/>},
     {id:"Base_Sepolia",label:"Base",icon:<svg width="20" height="20" viewBox="0 0 32 32"><circle cx="16" cy="16" r="16" fill="#0052FF"/><path d="M16 6C10.477 6 6 10.477 6 16s4.477 10 10 10c5.254 0 9.564-4.055 9.966-9.2H15.2v-1.6h10.8C25.564 10.455 21.254 6 16 6z" fill="white"/></svg>},
-    https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/arbitrum/info/logo.png
+    {id:"Arbitrum_Sepolia",label:"Arbitrum",icon:<img src="https://s2.coinmarketcap.com/static/img/coins/64x64/11841.png" width="20" height="20" style={{borderRadius:"50%"}}/>},
     {id:"Solana_Devnet",label:"Solana",icon:<img src="https://assets.coingecko.com/coins/images/4128/small/solana.png" width="20" height="20" style={{borderRadius:"50%"}}/>},
   ];
 
