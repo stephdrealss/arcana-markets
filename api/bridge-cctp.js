c603a560df40a078b385b740594388e81a80e480
stephdrealss <94701111+stephdrealss@users.noreply.github.com>
2026-05-26 04:34:59 +0100

    Update bridge-cctp.js

--- api/bridge-cctp.js
+++ api/bridge-cctp.js
@@ -146,8 +146,8 @@ module.exports = async function handler(req, res) {
     const burnTxId = await executeTx(
       sourceWalletId,
       TOKEN_MESSENGER_V2,
-      'depositForBurn(uint256,uint32,bytes32,address,bytes32,uint256,uint256)',
-      [usdcAmount, String(ARC_DOMAIN), mintRecipient, usdcAddress, '0x0000000000000000000000000000000000000000000000000000000000000000', '0', '0']
+      'depositForBurn(uint256,uint32,bytes32,address)',
+      [usdcAmount, String(ARC_DOMAIN), mintRecipient, usdcAddress]
     );
 
     const burnResult = await waitForTx(burnTxId);
